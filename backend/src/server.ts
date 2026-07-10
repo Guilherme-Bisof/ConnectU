13486d93040042f30f4882512935e1e00dcee242
Guilherme-Bisof <gui_bisof@hotmail.com>
2026-07-10 17:57:15 -0300

    fix(cors): allow PATCH method to fix TypeError Failed to fetch

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -22,7 +22,7 @@ app.use(helmet());
 app.use(
   cors({
     origin: ["http://localhost:3000", "https://connect-u-psi.vercel.app"],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
   }));
